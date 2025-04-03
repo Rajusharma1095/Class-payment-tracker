d4ee9751fa975e61bebddac0adc4d0c369e41888
Rajusharma1095 <rajusharma91238@gmail.com>
2025-04-03 16:06:23 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("paymentForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Payment Submitted! Admin will verify your details.");
+});
