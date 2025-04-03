9b677803f65079548aa422a7243d39e113e68757
Rajusharma1095 <rajusharma91238@gmail.com>
2025-04-03 16:31:05 +0000

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.getElementById("paymentForm").addEventListener("submit", function(event
         amount: document.getElementById("amount").value,
     };
 
-    fetch("https://script.google.com/macros/s/AKfycbwUiUOE5DZQVfyOniO3AKWekqF6WBGXKbWjkKBA49L7o-eKguDnIO_GquUzu4vS8S7E/exec", {  // Replace with your deployment URL
+    fetch("https://script.google.com/macros/s/AKfycbzg8d_ljSi58IkOlYPZfM2ueu47Ba29kcb5r-w2u_wCcHhyRU6nCeuEfBbkWDRBC3de-Q/exec", {  // Replace with your deployment URL
         method: "POST",
         body: JSON.stringify(data),
         headers: { "Content-Type": "application/json" }
